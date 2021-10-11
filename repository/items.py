21a5b2c3cefe1f65569bec616df442ad472142f8
mikhalberns <mikhalb90@gmail.com>
2021-10-11 19:16:10 +0300

    add repository and routers files

---
+++ repository/items.py
@@ -0,0 +1,46 @@
+from db_utils.database import db
+from db_utils.models import Item
+from bson.objectid import ObjectId
+from typing import Optional, List
+
+
+def create_item(item: Item):
+    new_item = str(db.items.insert_one(dict(item)).inserted_id)
+    item = db.items.find_one({"_id": ObjectId(new_item)})
+    item["_id"] = str(item["_id"])
+    return item
+
+
+def read_item(item_id: str):
+    item = db.items.find_one({"_id": ObjectId(item_id)})
+    item["_id"] = str(item["_id"])
+    return item
+
+
+def read_items(user: Optional[str] = None, tags: Optional[str] = None):
+    user_filter = {}
+    if user:
+        user_filter = {"user": user}
+    if tags:
+        user_filter = {"tags": tags}
+    if user and tags:
+        user_filter = {"user": user, "tags": tags}
+    items = []
+    for item in db.items.find(user_filter):
+        item["_id"] = str(item["_id"])
+        items.append(item)
+    return items
+
+
+def update_item(item_id: str, item: Item):
+    user_filter = {"_id": ObjectId(item_id)}
+    item = {k: v for k, v in dict(item).items() if v is not None}
+    update_item = {"$set": item}
+    db.items.update_one(user_filter, update_item)
+    return item
+
+
+def delete_item(item_id: str):
+    delete_item = {"_id": ObjectId(item_id)}
+    db.items.delete_one(delete_item)
+    return "deleted"
---
+++ routers/items.py
@@ -0,0 +1,37 @@
+from fastapi import APIRouter, status
+from db_utils.database import db
+from db_utils.models import Item
+from bson.objectid import ObjectId
+from typing import Optional, List
+from repository import items
+
+
+router = APIRouter(
+    tags=["items"],
+    prefix="/items"
+)
+
+
+@router.post("/", status_code=status.HTTP_201_CREATED)
+def create_item(item: Item):
+    return items.create_item(item)
+
+
+@router.get("/{item_id}")
+def read_item(item_id: str):
+    return items.read_item(item_id)
+
+
+@router.get("/")
+def read_items(user: Optional[str] = None, tags: Optional[str] = None):
+    return items.read_items(user, tags)
+
+
+@router.put("/{item_id}", status_code=status.HTTP_202_ACCEPTED)
+def update_item(item_id: str, item: Item):
+    return items.update_item(item_id, item)
+
+
+@router.delete("/{item_id}")
+def delete_item(item_id: str):
+    return items.delete_item(item_id)
