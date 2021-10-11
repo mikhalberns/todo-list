35b5d1f52bb009be63df29a3fd62e1cb7f953bbb
mikhalberns <mikhalb90@gmail.com>
2021-10-11 19:55:31 +0300

    untrack files

--- repository/items.py
+++ repository/items.py
@@ -1,12 +1,12 @@
 from db_utils.database import db
 from db_utils.models import Item
 from bson.objectid import ObjectId
-from typing import Optional
+from typing import Optional, List
 
 
 def create_item(item: Item):
-    new_item = str(db.items.insert_one(dict(item)).inserted_id)
-    item = db.items.find_one({"_id": ObjectId(new_item)})
+    new_item_id = str(db.items.insert_one(dict(item)).inserted_id)
+    item = db.items.find_one({"_id": ObjectId(new_item_id)})
     item["_id"] = str(item["_id"])
     return item
 
@@ -17,14 +17,13 @@ def read_item(item_id: str):
     return item
 
 
-def read_items(user: Optional[str] = None, tags: Optional[str] = None):
+def read_items(user: Optional[str] = None, tags: Optional[str] = None) -> List[str]:
     user_filter = {}
     if user:
-        user_filter = {"user": user}
+        user_filter['user'] = user
     if tags:
-        user_filter = {"tags": tags}
-    if user and tags:
-        user_filter = {"user": user, "tags": tags}
+        user_filter['tags'] = tags
+
     items = []
     for item in db.items.find(user_filter):
         item["_id"] = str(item["_id"])
@@ -35,12 +34,12 @@ def read_items(user: Optional[str] = None, tags: Optional[str] = None):
 def update_item(item_id: str, item: Item):
     user_filter = {"_id": ObjectId(item_id)}
     item = {k: v for k, v in dict(item).items() if v is not None}
-    update_item = {"$set": item}
-    db.items.update_one(user_filter, update_item)
+    updated_item = {"$set": item}
+    db.items.update_one(user_filter, updated_item)
     return item
 
 
 def delete_item(item_id: str):
-    delete_item = {"_id": ObjectId(item_id)}
-    db.items.delete_one(delete_item)
+    deleted_item = {"_id": ObjectId(item_id)}
+    db.items.delete_one(deleted_item)
     return "deleted"
